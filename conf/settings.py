9e88519901fd8b84f214722d8a0f51aad1ab2909
ArtProPy <89776481+ArtProPy@users.noreply.github.com>
2022-11-04 20:42:17 +0300

    Create django project. Added swagger. Added PostgreSQL db.

---
+++ conf/settings.py
@@ -0,0 +1,97 @@
+import os
+from pathlib import Path
+
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+SECRET_KEY = os.environ['SECRET_KEY']
+
+DEBUG = int(os.environ['DEBUG'])
+ALLOWED_HOSTS = [os.environ['ALLOWED_HOST']]
+
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+if DEBUG:
+    INSTALLED_APPS.extend(['rest_framework_swagger', 'drf_yasg'])
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'conf.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'conf.wsgi.application'
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['POSTGRES_DB'],
+        'HOST': os.environ['POSTGRES_HOST'],
+        'PORT': os.environ['POSTGRES_PORT'],
+        'USER': os.environ['POSTGRES_USER'],
+        'PASSWORD': os.environ['POSTGRES_PASSWORD'],
+    }
+}
+
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
---
+++ conf/urls.py
@@ -0,0 +1,46 @@
+from django.contrib import admin
+from django.urls import path, re_path
+
+from conf import settings
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    from drf_yasg import openapi
+    from drf_yasg.views import get_schema_view
+    from rest_framework import permissions
+
+    schema_view = get_schema_view(
+        openapi.Info(
+            title="Notification center API",
+            default_version='0.1.0',
+            description="Документация API",
+        ),
+        public=True,
+        permission_classes=(permissions.AllowAny,),
+    )
+
+    urlpatterns.extend(
+        [
+            re_path(
+                r'^doc(?P<format>\.json|\.yaml)$',
+                schema_view.without_ui(cache_timeout=0),
+                name='schema-json',
+            ),
+            path(
+                'doc/',
+                schema_view.with_ui('swagger', cache_timeout=0),
+                name='schema-swagger-ui',
+            ),
+            path(
+                'redoc/',
+                schema_view.with_ui('redoc', cache_timeout=0),
+                name='schema-redoc',
+            ),
+        ]
+    )
+
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
